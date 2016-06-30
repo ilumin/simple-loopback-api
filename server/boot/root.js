a8f03a4e74133c955dc0747171ccaf74a72cc69d
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-30 14:56:59 +0700

    ADD simple param

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,13 @@
 module.exports = function(server) {
-  // Install a `/` route that returns server status
   var router = server.loopback.Router();
+
+  // Install a `/` route that returns server status
   router.get('/', server.loopback.status());
+
+  // simple param
+  router.get('/test-param/:name', function(req, res) {
+    res.send('Hello, ' + req.param('name'));
+  });
+
   server.use(router);
 };
